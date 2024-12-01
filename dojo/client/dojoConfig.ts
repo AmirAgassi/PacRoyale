4f2f090b7a40c1c2e30cec476abc6d9172dffc8e
AmirAgassi <33383085+AmirAgassi@users.noreply.github.com>
2024-11-30 20:06:24 -0500

    Fix ports for windows

--- dojo/client/dojoConfig.ts
+++ dojo/client/dojoConfig.ts
@@ -4,4 +4,7 @@ import manifest from "../contract/manifest_dev.json";
 
 export const dojoConfig = createDojoConfig({
     manifest,
+    rpcUrl: "http://127.0.0.1:5050",
+    toriiUrl: "http://127.0.0.1:8080",
+    relayUrl: "/ip4/127.0.0.1/tcp/9090",
 });
